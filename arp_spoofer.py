71e5d7a3e648414cf8aabfff4bc9566728a17ce6
Sedatyf <gaxel@hotmail.fr>
2020-09-10 17:20:25 +0200

    Added docString, documentation, show_manufacturer()

--- arp_spoofer.py
+++ arp_spoofer.py
@@ -1,11 +1,22 @@
-from scapy.all import *
+from scapy.all import Ether, ARP, srp, send
 import os, sys
-import pyfiglet
+import requests
+import pyfiglet, termcolor
+from datetime import datetime
 
 def arp_scan(ip):
+    """Perform an arp scan on an ip or a range of ip
 
+    Args:
+        ip (str): The ip's victim or a range of ip
+
+    Returns:
+        list: a list of host who responded to our request
+    """
     result = []
     
+    # This condition calculate the number of host from the network's mask
+    # Or specify 1 if it's not a range of ips
     if "/" in ip:
         mask = ip.split("/")
         number_hosts = pow(2, (32 - int(mask[1])))
@@ -14,15 +25,36 @@ def arp_scan(ip):
         number_hosts = 1
     print(f"Starting ARP Scan on {mask[0]} with {number_hosts} hosts")
 
+    # Make a packet for an ARP request
     request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip)    
 
+    # srp() sends ARP request AND wait for the response
     ans, unans = srp(request, timeout=2, retry=1, verbose=0)
+    # Append ips, macs, and manufacturer in result list for those who responded
     for sent, received in ans:
-        result.append({'IP': received.psrc, 'MAC': received.hwsrc})    
+        result.append({'IP': received.psrc, 'MAC': received.hwsrc, 'manu': show_manufacturer(received.hwsrc)})    
 
     return result
 
+def show_manufacturer(mac):
+    """Make an API request to get manufacturer name from a mac address
+
+    Args:
+        mac (string): current mac address
+
+    Returns:
+        string: manufacturer's name
+    """
+    # URL to the API
+    mac_url = 'https://macvendors.co/api/%s'
+    # Make API GET request
+    r = requests.get(mac_url % mac)
+    # Convert response in a json type
+    json = r.json()
+    return json['result']['company']
+
 def show_help():
+    # Pyfiglet make some cool banner
     custom_fig = pyfiglet.Figlet(font='slant')
     print(custom_fig.renderText('ARP spoofer'))
     
@@ -38,6 +70,7 @@ def show_help():
     """)
 
 def main():
+    # Get options and arguments
     opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
     args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
 
@@ -50,18 +83,24 @@ def main():
         sys.exit()
     
     if "-s" in opts:
+        t1 = datetime.now()
         result = arp_scan(args[0])
 
+        # Header's table
         print('{0} {1:>18}'.format('IP', 'MAC'))
 
+        # Print result in a nice align way
         for mapping in result:
-            text = mapping['IP']
-            text2 = mapping['MAC']
+            ip = mapping['IP']
+            mac = mapping['MAC']
+            manufacturer = mapping['manu']
 
-            space = 34 - len(text)
-            f = '{0} {1:>%d}' % (space)
+            space = 34 - len(ip)
+            f = '{0} {1:>%d}    {2:<2}' % (space)
 
-            print(f.format(text, text2))
+            print(f.format(ip, mac, manufacturer))
+        t2 = datetime.now()
+        termcolor.cprint("Scanning Completed in " + str(t2-t1), "green")
     else:
         raise SystemExit(show_help())
 
